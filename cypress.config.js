1b36af306306e25d4866d5a40cae4a63077ee780
yallav <vijayakumar_yalla@yahoo.com>
2024-03-19 16:27:17 +0000

    Refactored e2e object in config file for encapsulating with corresponding configurations

--- cypress.config.js
+++ cypress.config.js
@@ -2,14 +2,14 @@ const { defineConfig } = require('cypress');
 const cucumber = require('cypress-cucumber-preprocessor').default;
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     baseUrl: 'https://www.bluecrestwellness.com/',
+    specPattern: 'cypress/e2e/features/*.feature',
+    reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on('file:preprocessor', cucumber());
       require('cypress-mochawesome-reporter/plugin')(on);
     },
-    specPattern: 'cypress/e2e/features/*.feature',
   },
 });
